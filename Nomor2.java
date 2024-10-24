26064518d38d7791c4a61c6f22e0e5c19c1a9af1
MardiniDwiPutri <mardinidwiputri9@gmail.com>
2024-10-24 14:46:28 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,72 @@
 public class Nomor2{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Masukkan nama produk): ");
+        int namaboneka = scanner.nextInt();
+
+        System.out.print("Masukkan nama produk ): ");
+        double hargaboneka = scanner.nextDouble();
+
+        String namaproduk = ""; // namaproduk
+        double hargaboneka = ""; // hargaboneka
+        
+        switch (namaproduk) {
+            case 1:
+                if (namaproduk) {
+                    namaproduk = "Labubu"; // Boneka Labubu
+                } else if (namaproduk) {
+                    namaproduk = "Jenglot"; // Boneka Labubu
+                } else {
+                    namaproduk = "Voodo"; // Boneka Labubu
+                }
+                break;
+            case 2:
+                if (namaproduk) {
+                    namaproduk = "Labubu";
+                } else if (berat <= 8.0) {
+                    namaproduk = "Jenglot";
+                } else {
+                    namaproduk = "Voodo";
+                }
+                break;
+            case 3:
+                if (namaproduk) {
+                    namaproduk = "Labubu";
+                } else if (namaproduk) {
+                    namaproduk = "Jenglot";
+                } else {
+                    namaproduk = "Voodo";
+                }
+                break;
+            
+        switch (hargaboneka) {
+            case 1:
+                if (hargaboneka) {
+                    Labubu = "30.000"; // Harga Boneka Labubu
+                } else if (hargaboneka) {
+                    Labubu = "25.000"; // Harga Boneka Labubu
+                } else {
+                    Labubu = "20.000"; // Harga Boneka Labubu
+                break;
+                }
+                case 2:
+                if (hargaboneka) {
+                    Jenglot = "30.000"; // Harga Boneka Jenglot
+                } else if (hargaboneka) {
+                    Jenglot = "25.000"; // Harga Boneka Jenglot
+                } else {
+                    Jenglot = "20.000"; // Harga Boneka Jenglot
+                break;
+
+                case 3:
+                if (hargaboneka) {
+                    Voodo = "30.000"; // Harga Boneka Voodo
+                } else if (hargaboneka) {
+                    voodo = "25.000"; // Harga Boneka Voodo
+                } else {
+                    Voodo = "20.000"; // Harga Boneka Voodo
+                break;
+        System.out.println("namaproduk: " + hargaboneka);
     }
 }
