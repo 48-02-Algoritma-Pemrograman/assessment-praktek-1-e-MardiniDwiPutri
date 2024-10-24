5cb83c1ee40e257218ad6fc72bbdb34bcfe9ae4c
MardiniDwiPutri <mardinidwiputri9@gmail.com>
2024-10-24 13:50:55 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,37 @@
 public class Nomor1{
     public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+
+        Integer=anggotaKeluarga;
+        int=tunjanganKesehatan;
+        Input= Pilihan;
+
+        System.out.print("Masukkan Jumlah anggotaKeluarga(==3juta): ");
+        int anggotaKeluarga = scanner.nextInt();
+
+        System.out.print("Masukkan Jumlah anggotaKeluarga(>= 3 juta): ");
+        int anggotaKeluarga= scanner.next integer();
+
+        if (anggotaKeluarga ==3 juta){
+            System.out.println("tunjanganKesehatan=100.000/jiwa");
+            return;
+
+            String anggotakeluarga = ""; // anggotakeluarga
+            String tunjanganKesehatan = ""; // tunjanganKesehatan
+
+            switch (anggotaKeluarga) {
+
+                if (anggotaKeluarga==3juta) {
+                    tunjanganKesehatan = "100.000"; // tunjanganKesehatan;
+                else if (anggotaKeluarga 3 jiwa pertama >=3juta) {
+                    tunjanganKesehatan = "100.000"; // tunjanganKesehatan;
+                else {
+                    (anggotaKeluarga sisanya >=3juta) {
+                        tunjanganKesehatan = "50.000"; // tunjanganKesehatan;
+                      }
+                break;
+
+                System.out.println("anggotaKeluarga: " + tunjanganKesehatan);
 
     }
 }
